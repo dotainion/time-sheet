8aa37307c62af035bb846326a0bd8e7523777bee
dotainion <58013018+dotainion@users.noreply.github.com>
2021-09-19 22:08:41 -0400

    refactoring

--- src/public/404Error/404Errror.js
+++ src/public/404Error/404Errror.js
@@ -1,26 +1,23 @@
 import React from 'react';
 import { adminWelcome } from '../../contents/Sentences';
 import { useStore } from '../../state/stateManagement/stateManagement';
+import { GiDeskLamp } from 'react-icons/gi';
 
 
 export const Page404 = () =>{
     const { onContinue } = useStore();
 
     return (
-        <div className="no-select">
-            <p className="admin-welcome">{adminWelcome}</p>
-            <div className="float-center ">
-                <p><h1>page not found error</h1></p>
-                <div className="centered inline">
-                    <button className="btn btn-hover" style={{
-                        backgroundColor:"dodgerblue",
-                        color:"white",
-                        fontSize:"20px",
-                        marginLeft:"-20%"
-                    }} onClick={onContinue}>Continue...</button>
-                </div>
+        <div className="no-select" style={{height:"100vh",color:"white",backgroundColor:"rgb(0,0,0,0.95)"}}>
+            <div className="float-center" style={{textAlign:"center"}}> 
+                <div style={{fontSize:"30px",backgroundColor:"white",color:"black"}}>{adminWelcome.toUpperCase().replace(".","")}</div>
+                
+                <div style={{marginTop:"10px",fontSize:"30px",backgroundColor:"black",border:"4px solid white"}}>Opps.. page not found</div>
+                <div style={{fontSize:"80px"}}>404</div>
+                
+                <GiDeskLamp style={{fontSize:"100px"}}/>
+                <div onClick={onContinue} className="label-hover" style={{marginTop:"40px",color:"orange"}}>GO HOME</div>
             </div>
-            
         </div>
     )
 }
--- src/public/404Error/NotAutherize.js
+++ src/public/404Error/NotAutherize.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import { adminWelcome } from '../../contents/Sentences';
+import { useStore } from '../../state/stateManagement/stateManagement';
 
 
 export const NotAutherize = () =>{
-    const history = useHistory();
+    const { onContinue } = useStore();
+
     return (
-        <div className="no-select">
-            <p className="admin-welcome">{adminWelcome}</p>
-            <h1 className="float-center ">Not autherize</h1>
+        <div className="no-select" style={{width:"100%",height:"100vh",color:"white",backgroundColor:"rgb(0,0,0,0.95)"}}>
+            <div className="float-center " style={{fontSize:"25px",textAlign:"center",fontWeight:"bold"}}>
+                <div style={{color:"black",backgroundColor:"white",marginBottom:"10px"}}>{adminWelcome.toUpperCase().replace(".","")}</div>
+                <div style={{border:"3px solid white"}}>
+                    <div style={{color:"black",backgroundColor:"white"}}>YOU ARE NOT AUTHORIZED</div>
+                    <div>TO ACCESS THIS PAGE</div>
+                </div>
+                <div onClick={onContinue} className="label-hover" style={{fontSize:"15px",color:"orange",marginTop:"20px"}}>GO HOME</div>
+            </div>
         </div>
     )
 }
